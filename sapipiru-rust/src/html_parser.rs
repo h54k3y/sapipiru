5d5020d843c99cb619840ed5bf0c7391abc77e87
Saki Okubo <s4k118@gmail.com>
2021-01-06 00:40:27 +0900

    added attributes

--- sapipiru-rust/src/html_parser.rs
+++ sapipiru-rust/src/html_parser.rs
@@ -44,12 +44,14 @@ pub mod handmade_html_parser {
         token_type: TokenType,
         tag_name: String,
         token_data: String,
+        tag_attribute: Vec<(String, String)>,
     }
 
     enum TokenizeState {  // TODO: Add state according to https://html.spec.whatwg.org/multipage/parsing.html#data-state
         Data,
         TagOpen,
         TagName,
+        TagAttribute,
         EndTagOpen,
     }
 
@@ -62,11 +64,10 @@ pub mod handmade_html_parser {
         let mut current_state = TokenizeState::Data;
         let mut current_token: Token = Default::default();
         let mut tokens: Vec<Token> = Vec::new();
-        let mut should_push_to_tag = false;
-        let mut should_push_to_data = false;
         let mut token_end_flag = false;
-        let mut is_tag_name_defined = false;   // TODO: need to change this value
         let mut count_comment_end_char: i32 = 0;
+        let mut tmp_string: String = Default::default();
+        let mut tmp_attribute_strings: (String, String) = Default::default();
         for i in original_html.as_str().chars() {
             match current_state {
                 TokenizeState::Data => {
@@ -78,7 +79,7 @@ pub mod handmade_html_parser {
                         if current_token.token_type != TokenType::Text {
                             current_token.token_type = TokenType::Text;
                         }
-                        should_push_to_data = true;
+                        current_token.token_data.push(i);
                     }
                 }
                 TokenizeState::TagOpen => {
@@ -89,46 +90,76 @@ pub mod handmade_html_parser {
                             current_token.token_type = TokenType::StratTag;
                         }
                         current_state = TokenizeState::TagName;
-                        should_push_to_tag = true;
+                        current_token.tag_name.push(i);
                     }
                 }
                 TokenizeState::TagName => {
                     if i == '>' {
+                        current_state = TokenizeState::Data;
+                        token_end_flag = true;
                         current_token.token_type = {
                             if (current_token.token_type == TokenType::StratTag) && current_token.tag_name.to_uppercase().starts_with("!DOCTYPE") {
                                 current_token.tag_name = String::from("doctype");
                                 TokenType::Doctype
-                            } else if (current_token.token_type == TokenType::StratTag) && (current_token.tag_name.to_uppercase().starts_with("COMMENT") || current_token.tag_name.starts_with("!--")) {
+                            } else if (current_token.token_type == TokenType::StratTag) && /*(current_token.tag_name.to_uppercase().starts_with("COMMENT") ||*/ current_token.tag_name.starts_with("!--") {
+                                current_token.token_data = current_token.tag_name;
+                                if count_comment_end_char == 2 {
+                                    //current_token.token_data = &current_token.tag_name.slice_chars(3,current_token.tag_name.len() - 3);
+                                    assert_eq!(Some('-'), current_token.token_data.pop());
+                                    assert_eq!(Some('-'), current_token.token_data.pop());
+                                } else {
+                                    // This ">" is part of the comment.
+                                    token_end_flag = false;
+                                    current_token.token_data.push(i);
+                                }
                                 current_token.tag_name = String::from("comment");
                                 TokenType::Comment
                             } else {
                                 current_token.token_type
                             }
                         };
-
-                        if (current_token.token_type != TokenType::Comment) || (count_comment_end_char == 2) {
-                            token_end_flag = true;
-                            current_state = TokenizeState::Data;
-                            if current_token.token_type == TokenType::Comment {
-                                assert_eq!(Some('-'), current_token.token_data.pop());
-                                assert_eq!(Some('-'), current_token.token_data.pop());
-                            }
-                        } else {
-                            is_tag_name_defined = true;
-                            should_push_to_data = true;
-                        }
                     } else if i ==' ' {
-                            is_tag_name_defined = true;
-                    } else if is_tag_name_defined == false {
-                        should_push_to_tag = true;
+                        if !current_token.tag_name.starts_with("!--") {
+                            current_token.token_type = 
+                                if current_token.tag_name.to_uppercase().starts_with("!DOCTYPE") {
+                                    current_token.tag_name = String::from("doctype");
+                                    TokenType::Doctype
+                                } else {
+                                    current_token.token_type
+                                };
+                            current_state = TokenizeState::TagAttribute;
+                        }
                     } else {
-                        should_push_to_data = true;
+                        // push to tag_name
+                        current_token.tag_name.push(i);
                     }
 
+                    // count continuous comment end tag
                     if i == '-' {
                         count_comment_end_char += 1;
                     } else {
                         count_comment_end_char = 0;
+                    }
+                }
+                TokenizeState::TagAttribute => {
+                    // duplicated code, need to refactor.
+                    if i == '>' {
+                        current_state = TokenizeState::Data;
+                        token_end_flag = true;
+                        tmp_string = Default::default();
+                        tmp_attribute_strings = Default::default();
+                    } else if i == '=' {
+                        tmp_attribute_strings.0 = tmp_string;
+                        tmp_string = Default::default();
+                    } else if i =='"' {
+                        if !tmp_string.is_empty() {
+                            tmp_attribute_strings.1 = tmp_string;
+                            current_token.tag_attribute.push(tmp_attribute_strings);
+                            tmp_attribute_strings = Default::default();
+                            tmp_string = Default::default();
+                        }
+                    } else if i !=' ' {
+                        tmp_string.push(i);
                     }
                 }
                 TokenizeState::EndTagOpen => {
@@ -136,18 +167,8 @@ pub mod handmade_html_parser {
                         current_token.token_type = TokenType::EndTag;
                     }
                     current_state = TokenizeState::TagName;
-                    should_push_to_tag = true;
+                    current_token.tag_name.push(i);
                 }
-            }
-
-            if should_push_to_tag == true {
-                current_token.tag_name.push(i);
-                should_push_to_tag = false;
-            }
-
-            if should_push_to_data == true {
-                current_token.token_data.push(i);
-                should_push_to_data = false;
             }
 
             if token_end_flag == true {
@@ -156,16 +177,16 @@ pub mod handmade_html_parser {
                     tokens.push(current_token.clone());
                     current_token = Default::default();
                 }
-                is_tag_name_defined = false;
                 token_end_flag = false;
             }
         }
-        
+
         // add EOF token
         let eof_token: Token = Token {
             token_type: TokenType::EndOfFile,
-            tag_name: String::from(""),
-            token_data: String::from(""),
+            tag_name: Default::default(),
+            token_data: Default::default(),
+            tag_attribute: Vec::new(),
         };
         tokens.push(eof_token);
         tokens
@@ -194,6 +215,15 @@ pub mod handmade_html_parser {
             result.push_str(&convert_tokentype_to_string(i.token_type));
             result.push_str("   TagName-> ");
             result.push_str(&i.tag_name);
+            result.push_str("   TagAttributes-> ");
+                for j in &i.tag_attribute {
+                    result.push_str(" [");
+                    result.push_str("type: ");
+                    result.push_str(&j.0);
+                    result.push_str(",  data: ");
+                    result.push_str(&j.1);
+                    result.push_str("] ");
+                };
             result.push_str("   TokenData-> ");
             result.push_str(&i.token_data);
             result.push_str("\n");
