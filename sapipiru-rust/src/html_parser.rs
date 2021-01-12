0032d649dea2849ca8115450d49d08ffc8bdc345
Saki Okubo <s4k118@gmail.com>
2021-01-12 23:25:27 +0900

    BeforeHead tmp

--- sapipiru-rust/src/html_parser.rs
+++ sapipiru-rust/src/html_parser.rs
@@ -414,14 +414,62 @@ pub mod handmade_html_parser {
                             previous_sibiling_idx: -1,
                             next_sibiling_idx: -1,
                         };
+                        current_node.child_nodes_idx.push(node.this_node_idx);
+                        current_node.first_child_idx = node.this_node_idx;
+                        current_node.last_child_idx = node.this_node_idx;
                         dom_tree.push(node);
+                        current_node = node;
                         mode = Mode::BeforeHead;
                     }
                 },
                 Mode::BeforeHead => {
+                    if (i.token_type == TokenType::StratTag) && (i.tag_name.to_uppercase() == "HEAD") {
+                        // TODO: will add attribute
+                        let mut element_node = ElementNode {
+                            namespace_uri: Default::default(),
+                            prefix: Default::default(),
+                            local_name: Default::default(),
+                            tag_name: i.tag_name.to_uppercase(),
+                            id: Default::default(),
+                            class_name: Default::default(),
+                            class_list: Default::default(),
+                            slot: Default::default(),
+                            attributes: Default::default(),
+                            shadow_root: Default::default(),
+                        };
+                        element_stack.push(element_node);
+
+                        let content = NodeContent {
+                            node_type: NodeType::Element,
+                            node_name: i.tag_name.to_uppercase(),
+                            base_uri: Default::default(),
+                            is_connected: false,
+                            owner_document: Default::default(),
+                            parent_element: Default::default(),
+                            node_value: Default::default(),
+                            text_content: Default::default(),
+                        };
+                        let node = DOMNode {
+                            node_content: content,
+                            this_node_idx: count,
+                            parent_node_idx: current_node.this_node_idx,
+                            child_nodes_idx: Vec::new(),
+                            first_child_idx: -1,
+                            last_child_idx: -1,
+                            previous_sibiling_idx: -1,
+                            next_sibiling_idx: -1,
+                        };
+                        current_node.child_nodes_idx.push(node.this_node_idx);
+                        current_node.first_child_idx = node.this_node_idx;
+                        current_node.last_child_idx = node.this_node_idx;
+                        dom_tree.push(node);
+                        current_node = node;
+                        mode = Mode::InHead;
+                    }
+                },
+                Mode::InHead => {
                     
                 },
-                Mode::InHead => ,
                 Mode::AfterHead => ,
                 Mode::InBody => ,
                 Mode::AfterBody => ,
