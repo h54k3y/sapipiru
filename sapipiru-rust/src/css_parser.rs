b26aea8f9d93f1f6bbc49bf93036bd9fbb73736a
Saki Okubo <s4k118@gmail.com>
2021-01-19 23:53:57 +0900

    tmp adding lazy_static

--- sapipiru-rust/src/css_parser.rs
+++ sapipiru-rust/src/css_parser.rs
@@ -6,11 +6,15 @@ pub mod handmade_css_parser {
     };
     use std::io::Read;
 
+    lazy_static! {
+        pub static ref CSSTEXT: String = String::new();
+    }
+
     use crate::ui::styling_window;
 
     pub fn get_css(path: String) {
         let mut css_text = String::new();
         reqwest::blocking::get(&path).unwrap().read_to_string(&mut css_text);
-        
+        *CSSTEXT = css_text;
     }
 }
--- sapipiru-rust/src/main.rs
+++ sapipiru-rust/src/main.rs
@@ -3,6 +3,9 @@ mod html_parser;
 mod css_parser;
 use crate::ui::styling_window;
 
+#[macro_use]
+extern crate lazy_static;
+
 fn main() {
     styling_window::initialize_window();
 }
